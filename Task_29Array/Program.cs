22bc66a52ceb500104bcaf4a03d84ca6675f7b0d
nikitarsh <nikitaroshmakov@gmail.com>
2022-08-19 00:01:57 +0400

    add task 29 random array 8 num

---
+++ Task_29Array/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// (на примерах демонстрация выводов 5 и 3 размерных массивов, вам же нужно сделать 8)
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr = new int[8]; 
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        
+        Random rnd = new Random();
+        int value = rnd.Next(10);
+        arr[i] = value;
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+RandomArray(arr);
